0e9233b0137d63a7ed7e4429a3b39641e2c56765
Raizan <reisuke.raizan@gmail.com>
2015-01-04 00:12:59 +0700

    In progress: Login and session maker.

--- application/controllers/login.php
+++ application/controllers/login.php
@@ -4,10 +4,29 @@ class Login extends CI_Controller {
 
     function __construct(){
         parent::__construct();
+        $this->load->model('customer');
     }
 
     function index(){
         $this->load->view('login.php');
+    }
+
+    function authorize(){
+        $email = $this->input->post('email');
+		$password = $this->input->post('password');
+
+		$auth = $this->customer->authorize($email, $password);
+        if ($auth == true){
+            $data = array(
+                'email' => $email,
+                'login' => TRUE
+            );
+
+            $this->session->set_userdata($data);
+        }
+        else {
+
+        }
     }
 
 }
