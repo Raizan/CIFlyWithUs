53a53e045f84fd64c0418e666876a9217334adbb
Raizan <reisuke.raizan@gmail.com>
2015-01-03 22:19:30 +0700

    Login page added. Register view updated.

---
+++ application/views/login.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>FlyWithUs</title>
+  <link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/login.css?body=1" media="all" rel="stylesheet" />
+<link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/register.css?body=1" media="all" rel="stylesheet" />
+<link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/reserve.css?body=1" media="all" rel="stylesheet" />
+<link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/application.css?body=1" media="all" rel="stylesheet" />
+
+  <meta content="authenticity_token" name="csrf-param" />
+<meta content="nmWA7pBkK0oCM5Y5h8NQMa1sowVRU64KM+k4/VPY5A0=" name="csrf-token" />
+</head>
+<body>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Fly With Us | Online Airplane Ticket Reservation</title>
+	<link href="<?php echo base_url();?>assets/stylesheets/main.css?body=1" media="screen" rel="stylesheet" />
+	<link href="<?php echo base_url();?>assets/stylesheets/nova.css?body=1" media="screen" rel="stylesheet" />
+	<link href="<?php echo base_url();?>assets/stylesheets/style1.css?body=1" media="screen" rel="stylesheet" />
+	<link href="<?php echo base_url();?>assets/stylesheets/font-awesome.min.css?body=1" media="screen" rel="stylesheet" />
+</head>
+<body id="page">
+	<script src="<?php echo base_url();?>assets/javascripts/jquery-1.11.2.js?body=1"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/bootstrap.min.js?body=1"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/main.js?body=1"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/modernizr-2.6.2-respond-1.1.0.min.js?body=1"></script>
+		<header>
+		<nav id="navbar">
+			<ul class="site-nav">
+				<!--<li id="main-logo"><img src="../images"  </li>-->
+				<li><a href="<?php echo base_url();?>index.php/login">Home</a></li>
+				<li>About</li>
+				<li>Contact</li>
+			</ul>
+			<ul class="login-nav">
+				<li><a href="<?php echo base_url();?>index.php/register">Sign Up</a></li>
+				<li><a data-remote="true" data-target="#login-box" data-toggle="modal" href="<?php echo base_url();?>index.php/login">Sign In</a></li>
+			</ul>
+		</nav>
+	</header>
+	<section class="page" id="home">
+			<div id="home-container">
+			<p class="text-center"><i> start your journey and</i><br><b>Fly With Us</b></p>
+			<div id="home-button">
+				<a class="register-window button text-center" href="<?php echo base_url();?>index.php/register">Sign Up</a>
+				<a class="button text-center" data-remote="true" data-target="#login-box" data-toggle="modal" href="<?php echo base_url();?>index.php/login">Sign In</a>
+			</div>
+			</div>
+			<ul class="cb-slideshow">
+	            <li><span>Image 01</span></li>
+	            <li><span>Image 02</span></li>
+	            <li><span>Image 03</span></li>
+	            <li><span>Image 04</span></li>
+	     	</ul>
+	</section>
+	<section class="page" id="about">
+	</section>
+	<section class="page" id="contact">
+	</section>
+<!--  Login form -->
+	<div id="login-box" class="modal hide fade in" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+    <div class="modal-header">
+        <i class="icon-remove" data-dismiss="modal" aria-hidden="true"></i>
+        <h4 id="myModalLabel">Sign In Form</h4>
+    </div>
+    <!--Modal Body-->
+    <div class="modal-body popup-login">
+    	<form accept-charset="UTF-8" action="<?php echo base_url();?>index.php/login/do" class="form-inline" method="post"><div style="display:none"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="nmWA7pBkK0oCM5Y5h8NQMa1sowVRU64KM+k4/VPY5A0=" /></div>
+            <input class="input-small" id="login_username" name="email" placeholder="email" type="text" />
+			<br>
+			<input class="input-small" id="login_password" name="password" placeholder="password" type="password" />
+      		<input class="button" name="commit" type="submit" value="submit" />
+</form>    </div>
+    <!--/Modal Body-->
+</div>
+<!--  /Login form -->
+</body>
+</html>
+
+</body>
+</html>
--- application/views/register.php
+++ application/views/register.php
@@ -1,103 +1,121 @@
 <!DOCTYPE html>
 <html>
 <head>
+  <title>FlyWithUs</title>
+  <link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/login.css?body=1" media="all" rel="stylesheet" />
+<link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/register.css?body=1" media="all" rel="stylesheet" />
+<link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/reserve.css?body=1" media="all" rel="stylesheet" />
+<link data-turbolinks-track="true" href="<?php echo base_url();?>assets/stylesheets/application.css?body=1" media="all" rel="stylesheet" />
+
+  <meta content="authenticity_token" name="csrf-param" />
+<meta content="nmWA7pBkK0oCM5Y5h8NQMa1sowVRU64KM+k4/VPY5A0=" name="csrf-token" />
+</head>
+<body>
+
+<!DOCTYPE html>
+<html>
+<head>
 	<title>Fly With Us | Online Airplane Ticket Reservation</title>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/stylesheets/font-awesome.min.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/stylesheets/style1.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/stylesheets/main.css" />
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url();?>assets/stylesheets/bootstrap.min.css" />
+	<link href="<?php echo base_url();?>assets/stylesheets/main.css?body=1" media="screen" rel="stylesheet" />
+	<link href="<?php echo base_url();?>assets/stylesheets/style1.css?body=1" media="screen" rel="stylesheet" />
+	<link href="<?php echo base_url();?>assets/stylesheets/font-awesome.min.css?body=1" media="screen" rel="stylesheet" />
+	<link href="<?php echo base_url();?>assets/stylesheets/bootstrap.min.css?body=1" media="screen" rel="stylesheet" />
 </head>
 <body class="long">
-	<script type="text/javascript" src="<?php echo base_url();?>assets/javascripts/jquery-1.11.2.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url();?>assets/javascripts/bootstrap.min.js"></script>
-	<script type="text/javascript" src="<?php echo base_url();?>assets/javascripts/main.js"></script>
-	<script type="text/javascript" src="<?php echo base_url();?>assets/javascripts/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/jquery-1.11.2.js?body=1"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/bootstrap.min.js?body=1"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/main.js?body=1"></script>
+	<script src="<?php echo base_url();?>assets/javascripts/modernizr-2.6.2-respond-1.1.0.min.js?body=1"></script>
 		<header>
 		<nav id="navbar">
 			<ul class="site-nav">
 				<!--<li id="main-logo"><img src="../images"  </li>-->
-				<li><a href"#">Home</a></li>
+				<li><a href="<?php echo base_url();?>index.php/login">Home</a></li>
 				<li>About</li>
 				<li>Contact</li>
 			</ul>
 		</nav>
 		</header>
+
 	<section class="navbar-down ">
 		<h1 class="font">Create <b class="font-yellow">an</b> Account</h1>
-		<form class="form-horizontal" method="post" action="<?php echo base_url();?>index.php/register/create">
+		<form class="form-horizontal" method="post">
+        <form accept-charset="UTF-8" action="<?php echo base_url();?>index.php/register/create" class="form-horizontal" method="post"><div style="display:none"><input name="utf8" type="hidden" value="&#x2713;" /><input name="authenticity_token" type="hidden" value="nmWA7pBkK0oCM5Y5h8NQMa1sowVRU64KM+k4/VPY5A0=" /></div>
+		<!--<form class="form-horizontal">-->
 			<fieldset>
 			<!-- Text input-->
 			<div class="control-group">
-			  <label class="control-label" for="nama">Nama Lengkap</label>
+                <label class="control-label" for="register_nama_lengkap">Nama lengkap</label>
+			  <!--<label class="control-label" for="nama">Nama Lengkap</label>-->
 			  <div class="controls">
-			    <input value="<?php echo set_value('nama'); ?>" id="nama" name="nama" type="text" placeholder="Batman bin Suparman" class="input-xlarge" required="">
-				<?php echo form_error('nama'); ?>
+			  	  <input class="input-xlarge" id="register_nama_lengkap" name="nama" placeholder="Nabila" type="text" />
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="control-group">
-			  <label class="control-label" for="alamat">Alamat</label>
+                <label class="control-label" for="register_alamat">Alamat</label>
 			  <div class="controls">
-			    <input value="<?php echo set_value('alamat'); ?>" id="alamat" name="alamat" type="text" placeholder="Jl. Example" class="input-xlarge" required="">
-				<?php echo form_error('alamat'); ?>
+			  	  <input class="input-xlarge" id="register_alamat" name="alamat" type="text" />
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="control-group">
-			  <label class="control-label" for="email">E-mail</label>
+                <label class="control-label" for="register_email">Email</label>
 			  <div class="controls">
-			    <input value="<?php echo set_value('email'); ?>" id="email" name="email" type="text" placeholder="example@123.com" class="input-xlarge" required="">
-				<?php echo form_error('email'); ?>
+			  	  <input class="input-xlarge" id="register_email" name="email" type="text" />
 			  </div>
 			</div>
 
 			<!-- Password input-->
 			<div class="control-group">
-			  <label class="control-label" for="password">Password</label>
+                <label class="control-label" for="register_password">Password</label>
 			  <div class="controls">
-			    <input id="password" name="password" type="password" placeholder="" class="input-xlarge" required="">
-				<?php echo form_error('password'); ?>
+			  	  <input class="input-xlarge" id="register_password" name="password" type="password" />
+			    <p class="help-block">Minimal 8 karakter alfanumerik</p>
 			  </div>
 			</div>
 
 			<!-- Password input-->
 			<div class="control-group">
-			  <label class="control-label" for="konfirmasi_password">Konfirmasi Password</label>
+                <label class="control-label" for="register_konfirmasi_password">Konfirmasi password</label>
+			  <!--<label class="control-label" for="konfirmasi_password">Konfirmasi Password</label>-->
 			  <div class="controls">
-			    <input id="konfirmasi_password" name="konfirmasi_password" type="password" placeholder="" class="input-xlarge" required="">
-				<?php echo form_error('konfirmasi_password'); ?>
+			      <input class="input-xlarge" id="register_konfirmasi_password" name="konfirmasi_password" type="password" />
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="control-group">
-			  <label class="control-label" for="nomor_identitas">Nomor Identitas</label>
+                <label class="control-label" for="register_nomor_identitas">Nomor identitas</label>
 			  <div class="controls">
-			    <input value="<?php echo set_value('nomor_identitas'); ?>" id="nomor_identitas" name="nomor_identitas" type="text" placeholder="" class="input-xlarge" required="">
-			    <p class="help-block">Petunjuk: Gunakan KTP / Kartu Pelajar / Kartu SIM / Paspor</p>
-				<?php echo form_error('nomor_identitas'); ?>
+			  	  <input class="input-xlarge" id="register_nomor_identitas" name="nomor_identitas" type="text" />
+			    <p class="help-block">KTP / Kartu Pelajar / Kartu SIM / Paspor</p>
 			  </div>
 			</div>
 
 			<!-- Text input-->
 			<div class="control-group">
-			  <label class="control-label" for="nomor_telepon">Nomor Telepon</label>
+                <label class="control-label" for="register_nomor_telepon">Nomor telepon</label>
 			  <div class="controls">
-			    <input value="<?php echo set_value('nomor_telepon'); ?>" id="nomor_telepon" name="nomor_telepon" type="text" placeholder="" class="input-xlarge" required="">
-				<?php echo form_error('nomor_telepon'); ?>
+			  	  <input class="input-xlarge" id="register_nomor_telepon" name="nomor_telepon" type="text" />
 			  </div>
 			</div>
 
 			<!-- Button -->
 			<div class="control-group">
-			  <label class="control-label" for="submit"></label>
+                <label class="control-label" for="register_submit">Submit</label>
 			  <div class="controls">
-			    <button id="submit" name="submit" class="button">Submit</button>
+			  	  <input class="button" name="commit" type="submit" value="submit" />
 			  </div>
 			</div>
 		</fieldset>
-		</form>
-
+		<!--</form>-->
+</form>
 	</section>
+</body>
+</html>
+
+</body>
+</html>
