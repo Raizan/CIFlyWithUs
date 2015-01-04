43e68b185d9a618f1166ddcd8104ad7aa2b1759b
Raizan <reisuke.raizan@gmail.com>
2015-01-05 03:06:17 +0700

    asd

--- application/views/cart.php
+++ application/views/cart.php
@@ -42,7 +42,7 @@
                 echo '<td>'.$detil[$i]["nama_penumpang"].'</td>';
                 echo '<td>'.$detil[$i]["nomor_identitas"].'</td>';
 
-                echo '<td><a class="button" href="'.base_url().'index.php/cart/delete?idj='.$detil[$i]["id_jadwal"].'&idr='.$detil[$i]["id_reservasi"].'">CANCEL</td>';
+                echo '<td><a class="button" href="'.base_url().'index.php/cart/delete?id='.$detil[$i]["id"].'">CANCEL</td>';
                 echo '</tr>';
             }
         ?>
