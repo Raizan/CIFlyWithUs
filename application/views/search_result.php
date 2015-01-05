86b6aa521b3ee459f1c6b17fff8d7a3e101fda1f
natsumiaya <natsumiaya@users.noreply.github.com>
2015-01-05 07:40:40 +0700

    update

--- application/views/search_result.php
+++ application/views/search_result.php
@@ -3,10 +3,12 @@
 <head>
 	<title>Fly With Us | Online Airplane Ticket Reservation</title>
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/stylesheets/main.css"/>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/stylesheets/bootstrap.min.css"/>
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/stylesheets/nova.css"/>
 	<link rel="stylesheet" href="<?php echo base_url();?>assets/stylesheets/style1.css"/>
-	<link rel="stylesheet" href="<?php echo base_url();?>assets/stylesheets/font-awesome.css"/>
+	<link rel="stylesheet" href="<?php echo base_url();?>assets/stylesheets/font-awesome.min.css"/>
 	<script src="<?php echo base_url(); ?>assets/javascripts/jquery-1.11.2.js"></script>
+	<script src="<?php echo base_url(); ?>assets/javascripts/jquery.tablesorter/jquery.tablesorter.js"></script>
 	<script src="<?php echo base_url(); ?>assets/javascripts/bootstrap.min.js"></script>
 	<script src="<?php echo base_url(); ?>assets/javascripts/main.js"></script>
 	<script src="<?php echo base_url(); ?>assets/javascripts/modernizr-2.6.2-respond-1.1.0.min.js"></script>
@@ -49,7 +51,7 @@
 					<th></th>
 				</tr>
 			</thead>
-			<tbody class="font2 search-body">
+			<tbody class="font3 search-body">
 				<?php
 					$i = 0;
 					for($i = 0; $i < sizeof($data); $i = $i + 1){
@@ -59,7 +61,7 @@
 						echo '<td>'.$data[$i]["tanggal_berangkat"].'</td>';
 						echo '<td>'.$data[$i]["tanggal_sampai"].'</td>';
 						echo '<td>'.$data[$i]["harga_satuan"].'</td>';
-						echo '<td><a class="button" href="'.base_url().'index.php/book?id='.$data[$i]["id_jadwal"].'">BOOK NOW!</td>';
+						echo '<td><a class="button2" href="'.base_url().'index.php/book?id='.$data[$i]["id_jadwal"].'">BOOK NOW!</td>';
 						echo '</tr>';
 						$i = $i + 1;
 					}
@@ -68,5 +70,11 @@
 		</table>
 	</div>
 	</section>
+	<script type="text/javascript">
+		$(document).ready(function() 
+    	{ 
+        $("#tablesorter-demo").tablesorter(); 
+    	} ); 
+	</script>
 </body>
 </html>
